225b6aa731c42da500051af42b7d9a65127c3066
kulsoom adnan <adnanirfan282@gmail.com>
2024-05-07 12:23:52 -0700

    changes inn ts file

--- index.ts
+++ index.ts
@@ -1,23 +1,29 @@
 #! /usr/bin/env node 
 
 import inquirer from "inquirer"
+import chalk from "chalk";
 
 let irregularNumber = Math.floor(Math.random() * 6 + 1  )
  
-console.log("welcome to the number guessing game ");
+console.log(chalk.magenta.italic.bold
+("\n\t -----------------------------------------------\n\t\twelcome to the number guessing game \n \t------------------------------------------------\n\t"));
 
 let result = await inquirer.prompt([
     { 
         name: "userEstimateNo",
         type: "number",
-        message: "please enter the number between 1 to 6: "
+        message:chalk.italic.yellow ("please enter the number between 1 to 6: ")
     },
 ])
 
 if(result.userEstimateNo == irregularNumber){
-    console.log("Yayy!, You did it.YOU GUESS the correct number ");
+    console.log(chalk.italic.green.bold("\t\nYayy!, You did it.YOU GUESS the correct number \n\t"));
 }else {
-    console.log("Opps!, NOT CORRECT TRY AGAIN");
+    console.log(chalk.italic.red.bold("\t\n Error: Opps!, NOT CORRECT TRY AGAIN\t\n"));
+
     
 }
+
+console.log(chalk.italic.bold.cyan("\t\tThank you!\n\t-----------------------------\n\t\t"));
+
 
